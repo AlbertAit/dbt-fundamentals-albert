111b0795bb02693541c5d96926c6d6d57eb6d28b
Albert Aitov <albertaitov@accentcare.com>
2022-02-25 19:49:31 +0000

    213

--- models/staging/jaffle_shop/stg_customers.sql
+++ models/staging/jaffle_shop/stg_customers.sql
@@ -1,11 +1,7 @@
-with customers as (
-
-    select
-        id as customer_id,
+ select
+        Id as customer_id,
         first_name,
         last_name
+    from {{source('jaffle_shop','customers')}}
 
-    from dbt_aaitov_staging.customers
 
-)Select *
-from customers
--- models/staging/jaffle_shop/stg_orders.sql
+++ models/staging/jaffle_shop/stg_orders.sql
@@ -1,12 +1,6 @@
-with orders as (
-    
-    select
+   select
         id as order_id,
         user_id as customer_id,
         order_date,
         status
-
-    from dbt_aaitov_staging.orders
-)
-
-select * from orders
+    from {{source('jaffle_shop','orders')}}
